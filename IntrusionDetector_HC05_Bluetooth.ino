47b0852fbbb5417a83fda23d2c0f42c4fee6a97d
HridayCodes <63054583+HridayCodes@users.noreply.github.com>
2020-04-04 12:51:09 +0530

    Add files via upload

---
+++ IntrusionDetector_HC05_Bluetooth.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(7,INPUT);
+}
+
+void loop(){
+  // put your main code here, to run repeatedly:
+  if(digitalRead(7)==1){
+    Serial.println("23");
+    delay(3000);
+  }
+}
